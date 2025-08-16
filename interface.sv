266b7ee0649278088d82e2f0b8a8c4d9302ee87d
vaishali-pai <vaishalipai9591@gmail.com>
2025-08-16 15:52:18 +0530

    Create interface.sv

---
+++ interface.sv
@@ -0,0 +1,9 @@
+interface comparator_if(input logic clk, reset);
+  logic ip1, ip2;
+  logic gt,eq,lt;
+  
+  /*clocking driver_cb @(posedge clk);
+    default input #1 output #1;
+  endclocking
+  */
+endinterface
